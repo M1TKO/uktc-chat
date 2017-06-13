d56483828b5cc16a93bd24209993674f6bacbf0c
jo2n64 <valio31032@gmail.com>
2017-06-13 16:20:45 +0300

    Add files via upload
    added a sendMessages function(not tested)

--- chatroomsController.js
+++ chatroomsController.js
@@ -1,12 +1,36 @@
-function createRoom(){
-
+var mysql = require('mysql');
+var db = require('../db');
+var bodyParser = require('body-parser');
+var msg = {
+		text: req.body.text
+		};
+function index(){
+    
 };
+
+function createRoom(req, res) {
+	name = req.body.name;
+	db.createRoom(name);
+	console.log("Room created!!!");
+};
+
 function sendInvitation(){
 
 };
-function sendMessage(){
 
+function sendMessage(){
+let length = db.getData(text, 'text', 'messages');
+		if (length > 0) return true;
+		else return false;
+		let profiles_Id =1;
+		db.query(queries.insertMsg, chatroom_Id[], profiles_Id[], text[]);
 };
+
 function chatRoomMessages(){
 
 };
+
+exports.index = index; 
+exports.createRoom = createRoom;
+exports.sendInvitation = sendInvitation;
+exports.sendMessage = sendMessage;
